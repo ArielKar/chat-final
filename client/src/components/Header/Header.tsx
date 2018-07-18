b2cfc9bf8bece649641995b3b14fc7b2ab7efa70
ArielK <arielkarmazin@gmail.com>
2018-07-18 08:33:58 +0300

    fixing header layout

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -42,15 +42,16 @@ class Header extends Component<any, any> {
                 <div className="logo">
                     <img src="./logo.png" alt="chat-logo"/>
                 </div>
-                {this.props.user ?
+
                 <div className="logged-user">
-                    <p>Welcome <span className="logged-user-name">{this.props.user.name}</span></p>
-                </div> : null}
+                    <p>Welcome  <span className="logged-user-name">{this.props.user ? this.props.user.name : 'Guest'}</span></p>
+                </div>
                 <div className="dots-wrapper" ref={this.menuElement} onFocus={this.openMenu} onBlur={this.closeMenu}
                      tabIndex={0}>
-                    <div className="dot"/>
-                    <div className="dot"/>
-                    <div className="dot"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M0 0h24v24H0z" fill="none"/>
+                        <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/>
+                    </svg>
                     {this.state.isMenuShown ?
                         <div className="menu">
                             <ul>
